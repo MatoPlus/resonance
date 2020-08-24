4e7d2c4ab8ce806f31dd3a422b010dc509e3ce35
MatoPlus <rixin.yang.2001@gmail.com>
2020-08-24 17:30:03 -0400

    Made menubar into a vue component

--- src/background.js
+++ src/background.js
@@ -5,7 +5,6 @@ import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
 import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer'
 const isDevelopment = process.env.NODE_ENV !== 'production'
 const { menu } = require("./menu");
-const path = require("path");
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -19,13 +18,12 @@ protocol.registerSchemesAsPrivileged([
 function createWindow() {
   // Create the browser window.
   win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    minWidth : 1200,
+    minHeight : 800,
     webPreferences: {
-      preload: path.join(__dirname, "/../src/preload.js"),
       // Use pluginOptions.nodeIntegration, leave this alone
       // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
-      nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION
+      nodeIntegration: true
     },
     frame: false // Remove frame to hide default menu
   })
---
+++ src/components/Menubar.vue
@@ -0,0 +1,136 @@
+<template>
+  <div id="menu-bar">
+    <div class="left" role="menu">
+      <button class="menubar-btn" id="menu-btn" @click="openMenu">
+        <fa-icon id="menu-icon" :icon="['fas', 'bars']" style="color: white"/>
+      </button>
+      <h5 id="title">Resonance</h5>
+    </div>
+    <div class="right">
+      <button class="menubar-btn" id="minimize-btn" @click="minimizeWindow">
+        <fa-icon id="minimize-icon" :icon="['fas', 'window-minimize']" style="color: white"/>
+      </button>
+      <button class="menubar-btn" id="max-unmax-btn" @click="maxUnmaxWindow">
+        <fa-icon id="max-unmax-icon" :icon="screenStateIcon" style="color: white"/>
+      </button>
+      <button class="menubar-btn" id="close-btn" @click="closeWindow">
+        <fa-icon id="close-icon" :icon="['fas', 'times']" style="color: white"/>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+const { remote, ipcRenderer } = window.require("electron");
+
+export default {
+  name: "Menubar",
+  data() {
+    return {
+      isMaximized: this.getCurrentWindow().isMaximized()
+    }
+  },
+  methods: {
+    getCurrentWindow: function() {
+      return remote.getCurrentWindow();
+    },
+    openMenu: function(e) {
+      let x = e.x;
+      let y = e.y;
+      ipcRenderer.send(`display-app-menu`, { x, y });
+    },
+    minimizeWindow: function() {
+      let browserWindow = this.getCurrentWindow();
+      if (browserWindow.minimizable) {
+        browserWindow.minimize();
+      }
+    },
+    maximizeWindow: function() {
+      let browserWindow = this.getCurrentWindow();
+      if (browserWindow.maximizable) {
+        browserWindow.maximize();
+      }
+    },
+    unmaximizeWindow: function() {
+      this.getCurrentWindow().unmaximize();
+    },
+    maxUnmaxWindow: function() {
+      let browserWindow = this.getCurrentWindow();
+      if (browserWindow.isMaximized()) {
+        browserWindow.unmaximize();
+      } else {
+        browserWindow.maximize();
+      }
+      this.isMaximized = browserWindow.isMaximized();
+    },
+    closeWindow: function() {
+      this.getCurrentWindow().close();
+    },
+  },
+  computed: {
+    screenStateIcon() {
+      console.log(this.isMaximized)
+      return this.isMaximized ? ['far', 'clone'] : ['far', 'square']
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+body {
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  color: white;
+}
+
+#menu-bar {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 30px;
+  background: #34475a;
+  -webkit-app-region: drag;
+  color: white;
+}
+
+#menu-bar > div {
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.menubar-btn {
+  -webkit-app-region: no-drag;
+}
+
+.container {
+  height: calc(100vh - 30px);
+  background: #34475ab0;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 2em;
+}
+
+button {
+  height: 100%;
+  padding: 0 15px;
+  border: none;
+  background: transparent;
+  outline: none;
+}
+
+button:hover {
+  background: rgba(221, 221, 221, 0.2);
+}
+
+#close-btn:hover {
+  background: rgb(255, 0, 0);
+}
+
+button i {
+  color: white;
+}
+</style>
